53c522b60eb5cd22fcf3be08df642046c1be86f7
Ulysses07 <musasevinc.007@gmail.com>
2025-11-24 17:03:11 +0300

    24.11-2

---
+++ UniCast.Core/Models/CaptureDevice.cs
@@ -0,0 +1,11 @@
+﻿namespace UniCast.Core.Models
+{
+    public class CaptureDevice
+    {
+        public string Name { get; set; } = "";
+        public string Id { get; set; } = ""; // DevicePath (SymLink)
+
+        // UI'da ComboBox içinde düzgün görünmesi için
+        public override string ToString() => Name;
+    }
+}
